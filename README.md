0b7a6fc12219d52bdca002c1bf4b2dc833e9bda6
Bigcat <18217127859@163.com>
2018-11-25 22:46:48 +0800

    加dockerfile文件

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-#### 基于小黄鸡语料的对话机器人
+## 基于小黄鸡语料的对话机器人
+### 一. 执行说明
+启动tf_serving服务
+```bash
+python run_tf_serving.py
+```
+ 启动对话机器人
+```bash
+pytrhon run_robot.py
+```
--- run_tf_serving.py
+++ run_tf_serving.py
@@ -2,3 +2,36 @@
 """
 启动tf_serving
 """
+import os
+
+DOCKER_BUILD = "docker build -t tensorflow/serving ."
+DOCKER_RUN = "docker run -t --rm -p 8501:8501 "
+DOCKER_FILE_PATH = "docker/"
+PB_MODEL_PATH = "/model/chat_bot_pb"
+
+
+def docker_build():
+    """ 构建docker镜像
+    :return:
+    """
+    os.chdir(DOCKER_FILE_PATH)
+    os.system(DOCKER_BUILD)
+
+
+def docker_run():
+    """ 启动docker容器
+    :return:
+    """
+    global DOCKER_RUN
+    os.chdir("..")
+    path = os.getcwd().replace("\\", "/")
+    v = '-v  "' + path + PB_MODEL_PATH + ':/models/chat_bot" '
+    e = "-e MODEL_NAME=chat_bot tensorflow/serving "
+    DOCKER_RUN += (v + e)
+    print(DOCKER_RUN)
+    os.system(DOCKER_RUN)
+
+
+if __name__ == '__main__':
+    docker_build()
+    docker_run()
